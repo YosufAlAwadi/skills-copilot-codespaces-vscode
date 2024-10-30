2d947f99b5098b7fcb223560f0158c4ea1ca629f
YosufAlAwadi <alawadi4yosuf@gmail.com>
2024-10-30 12:24:51 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,6 +1,11 @@
 function skillsmember() {
-  return {
-    restrict: 'E',
-    templateUrl: 'views/skills.html'
-  };
+    var member = {
+        name: "John Doe",
+        age: 25,
+        skills: ["HTML", "CSS", "JS"],
+        details: function() {
+            console.log(this.name + " is " + this.age + " years old. He knows " + this.skills);
+        }
+    }
+    member.details();
 }
